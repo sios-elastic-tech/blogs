39413d210d697a6c2489ce3bee0ee2eea1792f8b
Yuji-TAGAWA-sti <tagawa@sios.com>
2025-03-05 10:18:53 +0900

    再コミット

---
+++ 2025-03-bulk/src/bulk_from_txt.py
@@ -0,0 +1,103 @@
+"""
+Copyright (c) SIOS Technology, Inc. All rights reserved.
+
+MIT License
+
+text を読み込んで、bulk により 文章と登録するツール
+
+Usage: python bulk_from_txt.py chunked_textfilepath
+
+登録先のindex名などは、es_consts.py に記載しておく。
+"""
+
+from io import TextIOWrapper
+import os
+import sys
+import logging
+
+from typing import Iterable
+from dotenv import load_dotenv
+
+from elasticsearch import Elasticsearch
+
+from elastic.es_consts import INGEST_PIPELINE, SEARCH_INDEX
+from elastic.es_func import create_es_client, refresh_index, streaming_bulk_wrapper
+
+from common.setup_logger import setup_logger
+
+
+def initialize_es() -> Elasticsearch:
+  """
+  Elasticsearchの初期化を行う。
+  """
+  elasticsearch_endpoint: str = ''
+  write_api_key_encoded: str = ''
+  es_client: Elasticsearch = None
+
+  if load_dotenv(verbose=True):
+    logger.debug('load_dotenv success')
+
+    if 'elasticsearch_endpoint' in os.environ:
+      elasticsearch_endpoint = os.environ['elasticsearch_endpoint']
+
+      if 'write_api_key_encoded' in os.environ:
+        write_api_key_encoded = os.environ['write_api_key_encoded']
+
+  if write_api_key_encoded == '':
+    print('please set elasticsearch_endpoint and write_api_key_encoded in .env')
+    logger.error('please set elasticsearch_endpoint and write_api_key_encoded in .env')
+  else:
+    es_client = create_es_client(elasticsearch_endpoint, write_api_key_encoded)
+    logger.debug(f'{es_client.info()=}')
+
+  return es_client
+
+
+def data_generator(input_file:TextIOWrapper, index_name:str=SEARCH_INDEX, ingest_pipeline_name:str=INGEST_PIPELINE) -> Iterable:
+  """
+  ファイルを1行ずつ読み取り、ドキュメント登録可能な形式にして返す。
+  """
+  for chunk_no, content in enumerate(input_file):
+    # 末尾の改行文字を削除する。
+    line_content = content.rstrip('\r\n')
+
+    # 1行ずつ処理する。
+    yield {
+      '_index': index_name,
+      'pipeline': ingest_pipeline_name,
+      '_source': {
+        'chunk_no': chunk_no,
+        'content': f"{line_content}"
+      }
+    }
+
+
+def bulk_from_file(es_client:Elasticsearch, input_text_filename:str, index_name:str=SEARCH_INDEX, refresh:bool=False):
+  """
+  1つのtextファイルを1行ずつ読み込んで、elasticsearch にドキュメント登録する。
+  """
+
+  with open(file=input_text_filename, buffering=-1, encoding="utf-8") as input_file:
+
+    success_count = streaming_bulk_wrapper(es_client=es_client, actions=data_generator(input_file))
+    logger.info(f'{success_count=}')
+        
+    if refresh and success_count > 0:
+      refresh_index(es_client, index_name=index_name)
+
+
+# ----- main -----
+logger = setup_logger()
+logger = logging.getLogger(__name__)
+
+es_client = initialize_es()
+
+if es_client is None:
+  logger.error('Initialize is failed.')
+  exit()
+
+args: list[str] = sys.argv
+
+text_filepath: str = args[1]
+
+bulk_from_file(es_client=es_client, input_text_filename=text_filepath, index_name=SEARCH_INDEX, refresh=True)
---
+++ 2025-03-bulk/src/elastic/es_consts.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) SIOS Technology, Inc. All rights reserved.
+
+MIT License
+"""
+
+# 検索対象のインデックス(のエイリアス)
+SEARCH_INDEX = 'kakinosuke'
+
+# ドキュメント登録時のパイプライン名
+INGEST_PIPELINE = 'japanese-text-embeddings'
