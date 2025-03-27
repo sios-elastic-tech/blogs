edc8fe961a002784e818a321392aabff7b58334c
Yuji-TAGAWA-sti <tagawa@sios.com>
2025-03-28 08:06:42 +0900

    License.txt -> LICENSE .env の読み込み時の Verbose のデフォルト値を False に変更.

--- 2025-04-highlight/src/common/load_env_wrapper.py
+++ 2025-04-highlight/src/common/load_env_wrapper.py
@@ -11,16 +11,19 @@ from dotenv import load_dotenv
 ELASTICSEARCH_ENDPOINT : str = 'elasticsearch_endpoint'
 READ_API_KEY_ENCODED : str = 'read_api_key_encoded'
 
-def load_env_wrapper() -> Dict[str, str]:
+def load_env_wrapper(verbose:bool=False) -> Dict[str, str]:
     """
     .env ファイルを読み込み、読み込んだ結果を返す。
 
+    Parameters:
+        verbose (bool): 読み込み時のデバッグ情報を表示するか? (default=False)
+    
     Returns:
         Dict[str, str]: .env ファイルから読み込んだ結果
     """
     my_env: Dict[str, str] = {}
 
-    if load_dotenv(verbose=True):
+    if load_dotenv(verbose=verbose):
         required_vars = [
             ELASTICSEARCH_ENDPOINT,
             READ_API_KEY_ENCODED
